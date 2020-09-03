244c6016b9f9c7e79c04914dcebe4f895594b82f
Nadi.Pakhomchik <nadi_ka@mail.ru>
2020-09-03 13:00:16 +0300

    removing old files

--- jwd-parsers-project/src/main/java/by/epam/ts/controller/constant_attribute/RequestParameter.java
+++
@@ -1,9 +0,0 @@
-package by.epam.ts.controller.constant_attribute;
-
-public final class RequestParameter {
-	
-	public final static String SAX_PARSER = "sax_parser";
-	public final static String STAX_PARSER = "stax_parser";
-	public final static String DOM_PARSER = "dom_parser";
-
-}
--- jwd-parsers-project/src/main/java/by/epam/ts/dal/XmlDao.java
+++
@@ -1,7 +0,0 @@
-package by.epam.ts.dal;
-
-public interface XmlDao {
-	
-	byte[] readFromXmlFile(String xmlFileName) throws DaoException;
-
-}
