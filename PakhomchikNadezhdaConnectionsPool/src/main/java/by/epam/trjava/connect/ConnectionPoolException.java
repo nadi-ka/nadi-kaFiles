cf7f3a9a35ccff8f7b0c11638a12bd22b1288c35
Nadi.Pakhomchik <nadi_ka@mail.ru>
2020-09-03 12:23:33 +0300

    removing old files

--- PakhomchikNadezhdaConnectionsPool/src/main/java/by/epam/trjava/connect/ConnectionPoolException.java
+++
@@ -1,9 +0,0 @@
-package by.epam.trjava.connect;
-
-public class ConnectionPoolException extends Exception {
-    public static final long serialVersionUID = 1L;
-
-    public ConnectionPoolException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
